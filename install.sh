355fc2bcec86b795a70e0d02e7a28add2856eb45
Nishiki Liu <hello@nshki.com>
2021-07-30 13:22:07 -0700

    Update Brew installer command

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ elif [[ $os == "macos" ]]; then
   xcode-select --install
 
   # Install Homebrew and bundle.
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   brew bundle --file=$dev_dir/dotfiles/config/macos/Brewfile
 fi
 
