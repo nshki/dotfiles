e92732ed604663d6c56acb558a69406a5f0394ed
Nishiki Liu <hello@nshki.com>
2022-07-10 13:58:53 -0700

    Update install script to be only for ARM Macs

--- install.sh
+++ install.sh
@@ -5,55 +5,54 @@ read -p "Enter absolute path to dev directory [~/dev]: " dev_dir
 dev_dir=${dev_dir:-~/dev}
 mkdir -p $dev_dir
 
-# OS selection.
-while [[ $os != "debian" && $os != "macos" ]]; do
-  read -p "What OS are you using? [debian|macos]: " os
-done
-
 # Clone dotfiles repository.
 [ -d $dev_dir/dotfiles ] || git clone https://github.com/nshki/dotfiles.git $dev_dir/dotfiles
 
-# Install apps.
-if [[ $os == "debian" ]]; then
-  bash $dev_dir/dotfiles/config/debian/install_apps.sh
-elif [[ $os == "macos" ]]; then
-  # Ensure CLT for Xcode are installed.
-  xcode-select --install
+# Ensure CLT for Xcode are installed.
+xcode-select --install
 
-  # Install Homebrew and bundle.
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  brew bundle --file=$dev_dir/dotfiles/config/macos/Brewfile
-fi
+# Switch default shell to Homebrew Bash.
+chsh -s /opt/homebrew/bin/bash
 
-# Install tpm.
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+# Install Homebrew and bundle.
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew bundle --file=$dev_dir/dotfiles/config/Brewfile
 
-# Symlink and configure dotfiles.
+# Bash config.
 [ ! -f ~/.bash_local ] && touch ~/.bash_local
-mkdir -p ~/.config/nvim
-rm -f ~/.config/nvim/init.vim && ln -s $dev_dir/dotfiles/config/init.vim ~/.config/nvim/init.vim
-rm -f ~/.config/nvim/coc-settings.json && ln -s $dev_dir/dotfiles/config/coc-settings.json ~/.config/nvim/coc-settings.json
 rm -f ~/.bash_aliases && ln -s $dev_dir/dotfiles/config/.bash_aliases ~/.bash_aliases
-rm -f ~/.gitignore_global && ln -s $dev_dir/dotfiles/config/.gitignore_global ~/.gitignore_global
+rm -f ~/.bash_profile && ln -s $dev_dir/dotfiles/config/.bash_profile ~/.bash_profile
+source ~/.bash_profile
+
+# tmux and tpm config.
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 rm -f ~/.tmux.conf && ln -s $dev_dir/dotfiles/config/.tmux.conf ~/.tmux.conf
 rm -f ~/.tmuxline && ln -s $dev_dir/dotfiles/config/.tmuxline ~/.tmuxline
-if [[ $os == "debian" ]]; then
-  echo "set -o vi" >> ~/.bashrc
-elif [[ $os == "macos" ]]; then
-  rm -f ~/.bash_profile && ln -s $dev_dir/dotfiles/config/macos/.bash_profile ~/.bash_profile
-fi
 
-# Install `vim-plug` for Neovim.
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+# Programming languages.
+curl --proto "=https" --tlsv1.2 -sSf https://sh.rustup.rs | sh
+nodenv install 16.16.0
+nodenv global 16.16.0
+pyenv install 3.10.5
+pyenv global 3.10.5
+rbenv install 3.1.2
+rbenv global 3.1.2
+
+# Install LunarVim.
+bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
+
+# Install kitty.
+curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+git clone --depth 1 git@github.com:dexpota/kitty-themes.git ~/.config/kitty/kitty-themes
+rm -f ~/.config/kitty/theme.conf && ln -s ~/.config/kitty/kitty-themes/themes/OneDark.conf ~/.config/kitty/theme.conf
+rm -f ~/.config/kitty/kitty.conf && ln -s $dev_dir/dotfiles/config/kitty.conf ~/.config/kitty/kitty.conf
 
 # Setup Git.
 read -p "Enter Git name: " git_name
 read -p "Enter Git email: " git_email
 git config --global user.name "$git_name"
 git config --global user.email $git_email
-git config --global core.editor nvim
-git config --global core.excludesfile ~/.gitignore_global
+git config --global core.editor lvim
 
 # Setup SSH key with GitHub.
 #
@@ -63,11 +62,7 @@ if [ ! -f ~/.ssh/id_rsa ]; then
   ssh-keygen -t rsa -b 4096 -C $github_email
 fi
 eval "$(ssh-agent -s)"
-if [[ $os == "debian" ]]; then
-  xclip -sel clip < ~/.ssh/id_rsa.pub
-elif [[ $os == "macos" ]]; then
-  cp $dev_dir/dotfiles/config/sshconfig ~/.ssh/config
-  ssh-add -K ~/.ssh/id_rsa
-  pbcopy < ~/.ssh/id_rsa.pub
-fi
+cp $dev_dir/dotfiles/config/sshconfig ~/.ssh/config
+ssh-add -K ~/.ssh/id_rsa
+pbcopy < ~/.ssh/id_rsa.pub
 read -p "SSH key copied to clipboard. Add to GitHub (https://github.com/settings/keys). Press enter when done."
